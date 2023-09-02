de9cc6f38aa1b98288a62d0dfc5fba84b5b5d6f2
Randy <randyfabianbelliard191407@gmail.com>
2023-09-01 21:51:36 -0400

    Agregando el Required

--- Models/Prioridades.cs
+++ Models/Prioridades.cs
@@ -3,8 +3,9 @@ using System.ComponentModel.DataAnnotations;
 public class Prioridades{
 
     [Key]
-    public int PrioridadId { get; set; } 
-    public String Descripcion { get; set; } = string.Empty;
+    public int PrioridadId { get; set; }
+    [Required(ErrorMessage ="La descripcion es requerida")]
+    public string Descripcion { get; set; } = string.Empty;
     public int DiasCompromiso { get; set; }
 
 }
